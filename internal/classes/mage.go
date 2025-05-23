a4693c79e8f1b68e8a788ba6c61d6f9ed50c9952
Diogo L Andrade <dlourenco521@gmail.com>
2025-05-23 10:22:05 -0300

    Update mage.go

--- internal/classes/mage.go
+++ internal/classes/mage.go
@@ -14,11 +14,6 @@ type Mage struct {
 	Skill     skills.SkillMagic
 }
 
-<<<<<<< HEAD
-func (m *Mage) UseSkill(skill skills.SkillMagic) string {
-	if m.Manapool >= skill.GetManaCost() {
-		m.Manapool -= skill.GetManaCost()
-=======
 func NewMage(name string, level int, manapool int, equipment tools.Weapon, skill skills.SkillMagic) *Mage {
 	return &Mage{
 		Name:      name,
@@ -28,7 +23,6 @@ func NewMage(name string, level int, manapool int, equipment tools.Weapon, skill
 		Skill:     skill,
 	}
 }
->>>>>>> 3c00d569d9563e921d37ebc65d02139138aaca48
 
 func (m *Mage) UseSkill() string {
 	if m.Manapool >= m.Skill.GetManaCost() {
@@ -39,18 +33,6 @@ func (m *Mage) UseSkill() string {
 	return "Not enough mana!"
 }
 
-<<<<<<< HEAD
-func (m *Mage) Execute() string {
-	if m.Manapool >= skills.Spell.CostMana {
-		m.Manapool -= m.Skill.GetManaCost()
-
-		return fmt.Sprintf("%s %s (Mana remaining: %d)", m.Name, skills.SkillMagic.GetManaCost(m.Skill), m.Manapool)
-	}
-	return "Not enough mana!"	
-}
-
-=======
->>>>>>> 3c00d569d9563e921d37ebc65d02139138aaca48
 func (m *Mage) GetClassName() string {
 	return "Mage"
 }
